f813616665b27c6773759ae5e558a7fc556acd5e
GeorgeDzigo <mr.georgegamertv@gmail.com>
2020-12-27 17:37:05 +0200

    Web Structure

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/reset.css">
+
+      <!-- FONT -->
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+      <header>
+      <h1 class='logo'>Jobs Finder</h1>
+      
+      </header>
+      
+</body>
+</html>
