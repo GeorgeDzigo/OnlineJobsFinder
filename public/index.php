ef9089b61a17cfc94912578233853efc28b9414e
GeorgeDzigo <mr.georgegamertv@gmail.com>
2020-12-28 19:34:37 +0200

    add style and more pages

--- public/index.php
+++ public/index.php
@@ -9,15 +9,31 @@
 
       <!-- FONT -->
       <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bangers&display=swap" rel="stylesheet">
 
 </head>
 <body>
+      <!-- HEADER -->
+            <header class="header">
+                  <h1 class='logo'>Jobs Finder</h1>
 
-      <header>
-      <h1 class='logo'>Jobs Finder</h1>
-      
-      </header>
+                  <nav class="nav">
+                        <li class="nav-li"> <a href="./index.php" class="nav-li-a" style="color:lightslategray; font-weight:bold;">Vacancies</a></li>
+                        <li class="nav-li"> <a href="./add.php" class="nav-li-a">Publish Vacancies</a></li>
+                        <li class="nav-li"> <a href="#" class="nav-li-a">Contact</a></li>
+                  
+                  </nav>
+            </header>
+      <!-- END HEADER -->
+
+      <section class="search-job">
+            <center>
+                  <form action="#" method="POST" class="form">
+                  <input type="text" name="search" placeholder="Search For Jobs">
+                        <input type="submit" value="Search">
+                  </form>
+            </center>
+      </section>
       
 </body>
 </html>
