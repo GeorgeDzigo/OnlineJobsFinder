15fddd9d26933c6e4af2cb571563268131d61a17
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-01-02 14:52:39 +0200

    Inlucding HTML PARTS with PHP

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/reset.css">
+
+      <!-- FONT -->
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bangers&display=swap" rel="stylesheet">
+
+</head>
+<body>
--- public/index.php
+++ public/index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <link rel="stylesheet" href="css/style.css">
-      <link rel="stylesheet" href="css/reset.css">
-
-      <!-- FONT -->
-      <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bangers&display=swap" rel="stylesheet">
-
-</head>
-<body>
+<?php require '../header.php'?>
       <!-- HEADER -->
             <header class="header">
                   <h1 class='logo'>Jobs Finder</h1>
@@ -35,5 +21,4 @@
             </center>
       </section>
       
-</body>
-</html>
+<?php require '../footer.php'?>
