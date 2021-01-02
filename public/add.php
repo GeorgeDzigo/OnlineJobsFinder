1c362ebf96e95c863ab260908d51d8bfa47d34df
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-01-02 18:07:45 +0200

    Added form in add php and more

--- public/add.php
+++ public/add.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Your Vacancy</title>
-</head>
-<body>
-      
-</body>
-</html>
+<?php include '../header.php'?>
+      <div class="formholder">
+            <div class="headersholder">
+                  <h1 class="headersholder-h1">Fill Your Vacancy</h1>
+            </div>
+            <center>
+                  <form action="#" method="POST" class="add">
+                        <input type="text" name="firstname" id="fname" placeholder="First Name" >
+                        <input type="text" name="lastname" id="lname" placeholder="Last Name">
+                        <input type="text" name="companyname" id="cname" placeholder="Company Name">
+                        <input type="email" name="companyemail" id="cemail" placeholder="Company Email">
+                        <input type="text" name="zipcode" id="zcode" placeholder="Zip Code"/>
+                        <input type="tel" name="phonenumber" id="pnumber" placeholder="Phone Number">
+                        <input type="text" name="vacancyname" placeholder="Vacancy Name">
+                        <textarea name="desc" id="" cols="30" rows="10"></textarea>
+                        <input type="submit">
+                  </form>
+            </center>
+      </div>
+
+<?php include '../footer.php'?>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,4 @@
 <?php require '../header.php'?>
-      <!-- HEADER -->
-            <header class="header">
-                  <h1 class='logo'>Jobs Finder</h1>
-
-                  <nav class="nav">
-                        <li class="nav-li"> <a href="./index.php" class="nav-li-a" style="color:lightslategray; font-weight:bold;">Vacancies</a></li>
-                        <li class="nav-li"> <a href="./add.php" class="nav-li-a">Publish Vacancies</a></li>
-                        <li class="nav-li"> <a href="#" class="nav-li-a">Contact</a></li>
-                  
-                  </nav>
-            </header>
-      <!-- END HEADER -->
-
       <section class="search-job">
             <center>
                   <form action="#" method="POST" class="form">
