502cbba653a1cb32ffe8a647f7c4ec0ea76b3947
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-01-06 15:00:46 +0200

    Added SignOut

--- public/view/header.php
+++ public/view/header.php
@@ -1,12 +1,13 @@
+<?php
+      if(count($_SESSION) != 0) echo "<a href='./signout.php'>SIGN OUT</a>";
+?>
       <!-- HEADER -->
       <header class="header">
       <h1 class='logo'><a href="./index.php">Jobs Finder</h1>
-
                   <nav class="nav">
                         <li class="nav-li"> <a href="./index.php" class="nav-li-a">Vacancies</a></li>
                         <li class="nav-li"> <a href="./add.php?s=publish" class="nav-li-a">Publish Vacancies</a></li>
                         <li class="nav-li"> <a href="#" class="nav-li-a">Contact</a></li>
-                  
                   </nav>
       </header>
       <!-- END HEADER -->
